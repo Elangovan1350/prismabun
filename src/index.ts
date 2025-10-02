70711b33b06b2331728457d0bdf795ad5801f561
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-03 01:05:28 +0530

    change16

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { PrismaClient } from "../generated/client";
+import { PrismaClient } from "@prisma/client";
 
 const app = new Hono();
 const prisma = new PrismaClient();
